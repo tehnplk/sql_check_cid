da670ea35e73dee2f9317e6d427ffd1a95799e60
tehnplk <56914076+tehnplk@users.noreply.github.com>
2021-07-14 08:47:09 +0700

    Update check_cid.sql

--- check_cid.sql
+++ check_cid.sql
@@ -1,28 +1,28 @@
 select 
-data.cid
-,case when data.caldigit > 9 then data.caldigit - 10 else data.caldigit end caldigit
-,data.checkdigit
-,case when case when data.caldigit > 9 then data.caldigit - 10 else data.caldigit end = data.checkdigit then 'OK' else 'ERR' End chk
+t.cid
+,case when t.caldigit > 9 then t.caldigit - 10 else t.caldigit end caldigit
+,t.checkdigit
+,case when case when t.caldigit > 9 then t.caldigit - 10 else t.caldigit end = t.checkdigit then 'OK' else 'ERR' End result
 from (
-	 select t.cid,
+	 select cid,
 	 11-(
-	 (cast(substring(t.cid,1,1) as integer) * 13 +
-	 cast(substring(t.cid,2,1) as integer) * 12 +
-	 cast(substring(t.cid,3,1) as integer) * 11 +
-	 cast(substring(t.cid,4,1) as integer) * 10 +
+	 (cast(substring(cid,1,1) as integer) * 13 +
+	 cast(substring(cid,2,1) as integer) * 12 +
+	 cast(substring(cid,3,1) as integer) * 11 +
+	 cast(substring(cid,4,1) as integer) * 10 +
 
-	 cast(substring(t.cid,5,1) as integer) * 9 +
-	 cast(substring(t.cid,6,1) as integer) * 8 +
-	 cast(substring(t.cid,7,1) as integer) * 7 +
-	 cast(substring(t.cid,8,1) as integer) * 6 +
+	 cast(substring(cid,5,1) as integer) * 9 +
+	 cast(substring(cid,6,1) as integer) * 8 +
+	 cast(substring(cid,7,1) as integer) * 7 +
+	 cast(substring(cid,8,1) as integer) * 6 +
 
-	 cast(substring(t.cid,9,1)  as integer) * 5 +
-	 cast(substring(t.cid,10,1) as integer) * 4 +
-	 cast(substring(t.cid,11,1) as integer) * 3 +
-	 cast(substring(t.cid,12,1) as integer) * 2 )
+	 cast(substring(cid,9,1)  as integer) * 5 +
+	 cast(substring(cid,10,1) as integer) * 4 +
+	 cast(substring(cid,11,1) as integer) * 3 +
+	 cast(substring(cid,12,1) as integer) * 2 )
 		% 11) caldigit ,
 
-	 cast(substring(t.cid,13,1) as integer)  checkdigit
-	 from  your_table_name t
-	 where t.cid is not null and LENGTH(t.cid)=13
- ) data
+	 cast(substring(cid,13,1) as integer)  checkdigit
+	 from  log_queue
+	 where cid is not null and LENGTH(cid)=13
+ ) t
